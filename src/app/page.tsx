8e46f32a44dd58c04ed354e15b4d057678a40517
Robby Widyahartono <robby.widyahartono@komunal.co.id>
2024-12-14 19:38:31 +0700

    Assets amd metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
-import Hello from "./components/hello";
-
 export default function Home() {
-  console.log("WHich component am I?")
   return (
     <>
-      <h1 className="font-bold text-3xl">Welcome to NextJS</h1>
-      <Hello />
+      <h1 className="h1-bold text-cyan-300">Welcome to NextJS</h1>
+      <h1 className="font-space-grotesk">Welcome to NextJS</h1>
     </>
   );
 }
