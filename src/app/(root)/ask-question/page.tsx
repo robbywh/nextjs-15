430b6707cb1fcb885a8a9b09ddc61961aa0cf84a
Robby Widyahartono <robby.widyahartono@komunal.co.id>
2024-12-28 15:07:35 +0700

    implement tag input

--- src/app/(root)/ask-question/page.tsx
+++ src/app/(root)/ask-question/page.tsx
@@ -1,5 +1,17 @@
-const AskQuestion = () => {
-  return <div>AskQuestion</div>;
+import React from "react";
+
+import QuestionForm from "@/components/forms/QuestionForm";
+
+const AskAQuestion = () => {
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+
+      <div className="mt-9">
+        <QuestionForm />
+      </div>
+    </>
+  );
 };
 
-export default AskQuestion;
+export default AskAQuestion;
