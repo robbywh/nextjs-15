39676b5c273e76d17b4e61c0c3f0d359788cc184
Robby Widyahartono <robby.widyahartono@komunal.co.id>
2024-12-28 22:17:28 +0700

    implement fetch handler

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -5,8 +5,8 @@ import HomeFilter from "@/components/filters/HomeFilter";
 import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
-// import handleError from "@/lib/handlers/error";
-// import { ValidationError } from "@/lib/http-errors";
+import { api } from "@/lib/api";
+import handleError from "@/lib/handlers/error";
 
 const questions = [
   {
@@ -49,23 +49,22 @@ const questions = [
   },
 ];
 
-// const test = async () => {
-//   try {
-//     throw new ValidationError({
-//       title: ["Required"],
-//       tags: ['"JavaScript" is not a valid tag.'],
-//     });
-//   } catch (error) {
-//     return handleError(error);
-//   }
-// };
+const test = async () => {
+  try {
+    return await api.users.getAll();
+  } catch (error) {
+    return handleError(error);
+  }
+};
 
 interface SearchParams {
   searchParams: Promise<{ [key: string]: string }>;
 }
 
 const Home = async ({ searchParams }: SearchParams) => {
-  // await test();
+  const users = await test();
+
+  console.log(users);
 
   const { query = "", filter = "" } = await searchParams;
 
