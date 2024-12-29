84233209d1db45eeb2e286192873ca36de750350
Robby Widyahartono <robby.widyahartono@komunal.co.id>
2024-12-29 18:08:39 +0700

    implement edit question

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -10,7 +10,7 @@ const questions = [
   {
     _id: "1",
     title: "How to learn React?",
-    description: "I want to learn React, can anyone help me?",
+    content: "I want to learn React, can anyone help me?",
     tags: [
       { _id: "1", name: "React" },
       { _id: "2", name: "JavaScript" },
@@ -29,7 +29,7 @@ const questions = [
   {
     _id: "2",
     title: "How to learn JavaScript?",
-    description: "I want to learn JavaScript, can anyone help me?",
+    content: "I want to learn JavaScript, can anyone help me?",
     tags: [
       { _id: "1", name: "JavaScript" },
       { _id: "2", name: "JavaScript" },
